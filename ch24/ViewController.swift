1fbef573ac456d76c750ec4a9d57d3b9d8a2cf8d
Your Name <you@exmaple.com>
2018-02-21 21:12:45 +0900

    Realm database practice

--- ch24/ViewController.swift
+++ ch24/ViewController.swift
@@ -7,19 +7,38 @@
 //
 
 import UIKit
+import RealmSwift
+
+class Todo : Object { //realm 만들기 위해서는 Object를 상속받아야함.
+    @objc dynamic var title : String! //프로퍼티는 dynamic var로 선언
+    @objc dynamic var dueDate : Date!
+} //모델 생성
 
 class ViewController: UIViewController {
-
+ 
+    var realm : Realm! //데이터베이스 생성
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        realm = try! Realm() //객체 생성(throw가 있기 때문에 try!를 써주어야함.
+        print("Home : \(NSHomeDirectory())")
+        //print("Realm path: \(Realm.Configuration.defaultConfiguration.fileURL!)")
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewWillAppear(_ animated: Bool) {
+        add(title: "UIKit Study", due: Date())
+        add(title: "Network Study", due: Date())
     }
-
-
+    
+    func add(title : String, due date : Date) { //데이터 추가(데이터베이스에 저장)
+        
+        let todo = Todo() //todo 객체 생성
+        todo.title = title
+        todo.dueDate = date
+        
+        try! realm.write {
+            realm.add(todo) //데이터베이스에 반영됨
+        }
+    }
 }
 
